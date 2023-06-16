25916494ae228f6d30733175c8c0caee19c15040
Ouissalajabli <ouissalajabli@gmail.com>
2023-06-16 16:29:13 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers,
+ *              separated by ", ", in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+putchar(digit + '0');
+
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
