cc86317bbbf6c1eeefbedc87c8db88a74c2a36ad
Ouissalajabli <ouissalajabli@gmail.com>
2023-06-21 18:14:32 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,12 +16,12 @@ while (i < 24)
 j = 0;
 while (j < 60)
 {
-            putchar((i / 10) + '0');
-            putchar((i % 10) + '0');
-            putchar(':');
-            putchar((j / 10) + '0');
-            putchar((j % 10) + '0');
-            putchar('\n');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(':');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+putchar('\n');
 j++;
 }
 i++;
